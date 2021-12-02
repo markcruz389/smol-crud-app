a98090af46f1da410ea84577312f49b2fdf8e421
Joshua Turingan <turingan.joshua@gmail.com>
2021-12-02 21:10:09 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React, {useState} from 'react'
+
+const App = () => {
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
+  const [id, setId] = useState(-1)
+  const [listNames, setListNames] = useState([])
+  const [status, setStatus] = useState('add')
+
+  const handleSave = () => {
+    if(status === 'add') {
+      setListNames([...listNames, { id: listNames.length, firstName, lastName }])
+    } else if(status === 'edit') {
+      const editName = listNames.find(name => name.id === id)
+      setListNames(listNames.map(name => {
+        if(name.id === editName.id) {
+          return {
+            id,
+            firstName,
+            lastName
+          }
+        }
+        return name
+      }))
+    }
+
+    setFirstName('')
+    setLastName('')
+    setStatus('add')
+  }
+
+  const handleEdit = (val) => {
+    const {id, firstName, lastName} = val
+    setFirstName(firstName)
+    setLastName(lastName)
+    setId(id)
+    setStatus('edit')
+  }
+
+  const handleDelete = (id) => {
+    setListNames(listNames.filter(name => {
+      return name.id !== id
+    }))
+
+    setFirstName('')
+    setLastName('')
+    setId(-1)
+    setStatus('add')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <div>{status}</div>
+        <label>first name</label>  
+        <input type='text' value={firstName} onChange={(e) => {
+          const {value} = e.target
+          setFirstName(value)
+        }} />
+      </div>
+      <div>
+        <label>last name</label>  
+        <input type='text' value={lastName} onChange={e => {
+          const {value} = e.target
+          setLastName(value)
+        }} />
+      </div>
+      <button onClick={handleSave}>Save</button>
+      <br />
+      <br />
+      <br />
+      <ul>
+        {listNames.map((name) => (
+          <li 
+            key={name.id}
+            onClick={() => handleEdit(name)}
+          >
+            {`${name.id} ${name.firstName} ${name.lastName}`} <button onClick={() => handleDelete(name.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
